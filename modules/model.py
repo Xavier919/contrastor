04f357dc39f0bbd973ebfb9d80de6e52a9eb6ea5
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-03-31 12:15:40 -0400

    up modules

--- modules/model.py
+++ modules/model.py
@@ -48,12 +48,12 @@ class BaseNet1D(nn.Module):
         block = nn.Sequential(
             nn.Conv1d(in_channels, in_channels, kernel_size=k, groups=in_channels, padding='same'),
             nn.Conv1d(in_channels, out_channels, kernel_size=1, padding='same'),
-            nn.SELU(),
-            nn.BatchNorm1d(out_channels),
+            nn.GELU(),
+            #nn.BatchNorm1d(out_channels),
             nn.Conv1d(out_channels, out_channels, kernel_size=k, groups=out_channels, padding='same'),
             nn.Conv1d(out_channels, out_channels, kernel_size=1, padding='same'),
-            nn.SELU(),
-            nn.BatchNorm1d(out_channels),
+            nn.GELU(),
+            #nn.BatchNorm1d(out_channels),
         )
         return block
         
@@ -61,8 +61,8 @@ class BaseNet1D(nn.Module):
     def final_block(in_channels, out_channels, k=1):
         block = nn.Sequential(
             nn.Conv1d(in_channels, out_channels, kernel_size=k, padding='same'),
-            nn.SELU(),
-            nn.BatchNorm1d(out_channels),
+            nn.GELU(),
+            #nn.BatchNorm1d(out_channels),
         )
         return block
 
