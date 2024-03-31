680b6ac9f0939563c566f1e8f4f2911ed9a888fb
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-03-30 21:08:48 -0400

    up modules

--- modules/model.py
+++ modules/model.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from modules.utils import euclid_dis
 
 class BaseNet1D(nn.Module):
-    def __init__(self, input_channels, sequence_length):
+    def __init__(self, input_channels):
         super(BaseNet1D, self).__init__()
         self.maxpool = nn.MaxPool1d(kernel_size=2)
         self.conv1 = self.conv_block(input_channels, 64, k=5)
@@ -11,7 +11,6 @@ class BaseNet1D(nn.Module):
         self.conv3 = self.conv_block(32, 16, k=5)
         self.conv4 = self.conv_block(16, 8, k=5)
         self.dconvf = self.final_block(8, 1)
-        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         block1 = self.conv1(x)
@@ -23,7 +22,7 @@ class BaseNet1D(nn.Module):
         block4 = self.conv4(x) 
         x = self.maxpool(block4)
         x = self.dconvf(x)
-        return self.sigmoid(x)
+        return x
 
     @staticmethod
     def conv_block(in_channels, out_channels, k=5):
