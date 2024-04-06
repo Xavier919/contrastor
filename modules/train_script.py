42f388d9b7787d6fcfca1998055b2d56dc7eb9eb
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-04-06 14:12:47 -0400

    up modules

--- modules/train_script.py
+++ modules/train_script.py
@@ -6,7 +6,6 @@ from gensim.models import KeyedVectors
 from modules.dataloader import PairedWord2VecDataset
 from torch.utils.data import DataLoader, DistributedSampler
 import torch.optim as optim
-from torch import nn
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
 from modules.transformer_model import BaseNetTransformer, SiameseTransformer
@@ -65,13 +64,14 @@ if __name__ == "__main__":
 
     optimizer = optim.RMSprop(siamese_model.parameters(), lr=args.lr)
 
-    print(f'Number of pairs: {args.num_pairs}')
-    print(f'Batch_size: {args.batch_size}')
-    print(f'Epochs: {args.epochs}')
-    print(f'Hidden dimensions: {args.hidden_dim}')
-    print(f'Number of layers: {args.num_layers}')
-    print(f'Number of heads: {args.num_heads}')
-    print(f'Split: {args.split}')
+    if dist.get_rank() == 0:
+        print(f"Number of pairs: {args.num_pairs}")
+        print(f"Batch_size: {args.batch_size}")
+        print(f"Epochs: {args.epochs}")
+        print(f"Hidden dimensions: {args.hidden_dim}")
+        print(f"Number of layers: {args.num_layers}")
+        print(f"Number of heads: {args.num_heads}")
+        print(f"Split: {args.split}")
 
     epochs = args.epochs
     best_accuracy = 0
